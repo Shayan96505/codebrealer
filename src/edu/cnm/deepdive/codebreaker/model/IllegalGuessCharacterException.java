c8f4c87e70c403ffcd0c3a50ec1fb4e3646e80a4
Shayan <sgolafshani@cnm.edu>
2020-10-12 20:23:56 -0600

    Finished Java Docs comments for the exception classes

--- src/edu/cnm/deepdive/codebreaker/model/IllegalGuessCharacterException.java
+++ src/edu/cnm/deepdive/codebreaker/model/IllegalGuessCharacterException.java
@@ -1,11 +1,15 @@
 package edu.cnm.deepdive.codebreaker.model;
 
 /**
- * An exception that extends {@link IllegalArgumentException} for the incorrect use of pool of
+ * An exception class that extends {@link IllegalArgumentException} for the incorrect use of pool of
  * characters.
  */
 public class IllegalGuessCharacterException extends IllegalArgumentException {
 
+  /**
+   * Different overloaded methods that return a message, the cause, or both of an
+   * IllegalGuessCharacter exception and handle the exception.
+   */
   public IllegalGuessCharacterException() {
   }
 
--- src/edu/cnm/deepdive/codebreaker/model/IllegalGuessLengthException.java
+++ src/edu/cnm/deepdive/codebreaker/model/IllegalGuessLengthException.java
@@ -1,7 +1,15 @@
 package edu.cnm.deepdive.codebreaker.model;
 
+/**
+ * An exception class that extends {@link IllegalArgumentException} for the incorrect guess length
+ */
+
 public class IllegalGuessLengthException extends IllegalArgumentException {
 
+  /**
+   * Different overloaded methods that return a message, the cause, or both of an IllegalGuessLength
+   * exception and handle the exception.
+   */
   public IllegalGuessLengthException() {
   }
 
